06f3509f93964f4f5fd2e90552f8849e26a04551
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-12-06 21:33:04 -0600

    error

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 	
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library( name: "SwiftGameColorLibrary", targets: ["GameColor"])
+        .library( name: "GameColor", targets: ["GameColor"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
