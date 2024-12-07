bfd07ae598b4abdb9e7ced256a388803e3cecb7e
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-12-06 21:30:01 -0600

    error

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftGameColor",
+    name: "SwiftGameColorLibrary",
 	
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library( name: "SwiftGameColor", targets: ["Color"]),
+        .library( name: "SwiftGameColorLibrary", targets: ["GameColor"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,6 +17,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target( name: "Color", dependencies: []),
+        .target( name: "GameColor", dependencies: [])
    ]
 )
